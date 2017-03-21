258c37b75c62a1a2bef0c2bf6603dbc7f2e7a861
christophn <christoph.niemann@maibornwolff.de>
2017-03-21 09:00:36 +0100

    Remove deployment of parser

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 var JsmnSolLib = artifacts.require('./JsmnSolLib.sol');
-var Parser = artifacts.require('./ParserJson.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(JsmnSolLib);
-  deployer.link(JsmnSolLib, Parser);
-  deployer.deploy(Parser);
 };
