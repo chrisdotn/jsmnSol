0848026893ab34a2d4db8c533e3b66189917f1b8
christophn <christoph.niemann@maibornwolff.de>
2017-03-21 09:36:30 +0100

    Add Umlaut test

---
+++ test/TestUnicode.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.5;
+
+import "truffle/Assert.sol";
+import "../contracts/JsmnSolLib.sol";
+
+contract TestUnicode {
+
+    uint constant RETURN_SUCCESS = 0;
+    uint constant RETURN_ERROR_INVALID_JSON = 1;
+    uint constant RETURN_ERROR_PART = 2;
+    uint constant RETURN_ERROR_NO_MEM = 3;
+
+    function testUmlaut() {
+        string memory json = '{"key": "Möhrenbrot"}';
+
+        uint returnValue;
+        JsmnSolLib.Token[] memory tokens;
+        uint actualNum;
+
+        (returnValue, tokens, actualNum) = JsmnSolLib.parse(json, 5);
+
+        JsmnSolLib.Token memory t = tokens[2];
+
+        Assert.equal(returnValue, RETURN_SUCCESS, 'Valid JSON should return a success.');
+        Assert.equal(JsmnSolLib.getBytes(json, t.start, t.end), 'Möhrenbrot', 'Problems with an umlaut');
+    }
+
+}
