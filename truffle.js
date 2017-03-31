60fd3088d948177a12a7394ddcf7125e0958bec5
christophn <christoph.niemann@maibornwolff.de>
2017-03-31 17:38:06 +0200

    Add ropsten network config

--- truffle.js
+++ truffle.js
@@ -1,5 +1,16 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// 12-word mnemonic
+var mnemonic = "onyx aloof polio bronco spearfish clustered refined earflap darkroom slashing casualty curled";
+
 module.exports = {
     networks: {
+        ropsten: {
+            network_id: 3,      // Official ropsten network id
+            //provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),   // Use our custom provider
+            host: 'localhost',
+            port: 8545
+        },
         development: {
             host: "localhost",
             port: 8545,
