ba42be018a2af00f049d52f11b7f018731850f60
Shuja Ahmed <shuja.ahmed@ideatolife.me>
2018-05-16 00:46:07 +0400

    Updated JsmnSolLib to Solditiy 0.4.23
    Use of var was deprecated post Solidity v0.4.20. So updated the  core library along with one test case.

--- test/TestObjects.sol
+++ test/TestObjects.sol
@@ -1,28 +1,32 @@
-pragma solidity ^0.4.5;
+pragma solidity ^0.4.23;
 
 import "truffle/Assert.sol";
 import "../contracts/JsmnSolLib.sol";
 
 contract TestObjects {
-
+    
     uint constant RETURN_SUCCESS = 0;
     uint constant RETURN_ERROR_INVALID_JSON = 1;
     uint constant RETURN_ERROR_PART = 2;
     uint constant RETURN_ERROR_NO_MEM = 3;
-
+    
     function testSimpleObject() public {
-        string memory json = '{"outerKey": {"innerKey": "value"}}';
-
+        string memory json = '{"supplier_company_name":"Item description","supplier_authorized_person_name":"100","supplier_authorized_person_email":"Ton","supplier_company_street":"Steel Work","supplier_company_mobile_phone":"EUR900","supplier_company_fax":"EUR4500","supplier_account_beneficiary_name":"shuja","supplier_bank_name":"Emirates NBD"}';
+        
         uint returnValue;
         JsmnSolLib.Token[] memory tokens;
         uint actualNum;
-
-        (returnValue, tokens, actualNum) = JsmnSolLib.parse(json, 5);
-
-        JsmnSolLib.Token memory t = tokens[2];
-
+        
+        (returnValue, tokens, actualNum) = JsmnSolLib.parse(json, 101);
+        
+        JsmnSolLib.Token memory t = tokens[4];
+        string memory jsonElement = JsmnSolLib.getBytes(json, t.start, t.end);
+        
         Assert.equal(returnValue, RETURN_SUCCESS, 'Valid JSON should return a success.');
-        Assert.isTrue(t.jsmnType == JsmnSolLib.JsmnType.OBJECT, 'Not an object');
+        Assert.equal(jsonElement, '100', 'Valid JSON should return a success.');
+        Assert.equal(actualNum, 17, 'Should have returned the correct # of elements');
+        Assert.isTrue(t.jsmnType == JsmnSolLib.JsmnType.STRING, 'Not an string');
+        
+        
     }
-
 }
