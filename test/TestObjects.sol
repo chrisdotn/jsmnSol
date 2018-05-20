dee6781efe89446beb6b3f9bab3d6d072ec0254d
Shuja Ahmed <shuja.ahmed@ideatolife.me>
2018-05-20 22:27:28 +0400

    Updated the test case

--- test/TestObjects.sol
+++ test/TestObjects.sol
@@ -11,20 +11,18 @@ contract TestObjects {
     uint constant RETURN_ERROR_NO_MEM = 3;
     
     function testSimpleObject() public {
-        string memory json = '{"supplier_company_name":"Item description","supplier_authorized_person_name":"100","supplier_authorized_person_email":"Ton","supplier_company_street":"Steel Work","supplier_company_mobile_phone":"EUR900","supplier_company_fax":"EUR4500","supplier_account_beneficiary_name":"shuja","supplier_bank_name":"Emirates NBD"}';
+        string memory json = '{"outerKey": {"innerKey": "value"}}';
         
         uint returnValue;
         JsmnSolLib.Token[] memory tokens;
         uint actualNum;
         
-        (returnValue, tokens, actualNum) = JsmnSolLib.parse(json, 101);
+        (returnValue, tokens, actualNum) = JsmnSolLib.parse(json, 20);
         
         JsmnSolLib.Token memory t = tokens[4];
         string memory jsonElement = JsmnSolLib.getBytes(json, t.start, t.end);
         
         Assert.equal(returnValue, RETURN_SUCCESS, 'Valid JSON should return a success.');
-        Assert.equal(jsonElement, '100', 'Valid JSON should return a success.');
-        Assert.equal(actualNum, 17, 'Should have returned the correct # of elements');
         Assert.isTrue(t.jsmnType == JsmnSolLib.JsmnType.STRING, 'Not an string');
         
         
